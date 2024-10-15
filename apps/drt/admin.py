7a64205b16a53b55dbf47e18388568d249783226
Setayesh Sanavi <ssanavi@uoguelph.ca>
2024-10-15 11:15:55 -0400

    modify admin and migrations based on changes in model.py

--- apps/drt/admin.py
+++ apps/drt/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Owner, Requestor, NLink, Negotiation, Archive, SummaryStatistics, Questionnaire
-
+from .models import (
+    Owner, Requestor, NLink, Negotiation, Archive, 
+    SummaryStatistics, Questionnaire
+)
 
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
@@ -9,34 +11,38 @@ class OwnerAdmin(admin.ModelAdmin):
 
 @admin.register(Requestor)
 class RequestorAdmin(admin.ModelAdmin):
-    list_display = ['requestor_id', 'email', 'is_verified']  # Display fields that exist in Requestor model
-    search_fields = ['email']
-    list_filter = ['is_verified']
-
-@admin.register(NLink)
-class NLinkAdmin(admin.ModelAdmin):
-    list_display = ['link_id', 'owner', 'requestor', 'created_at', 'expired']
-    list_filter = ['expired', 'created_at']
-    search_fields = ['owner__name', 'requestor__email']
-
-@admin.register(Negotiation)
-class NegotiationAdmin(admin.ModelAdmin):
-    list_display = ['negotiation_id', 'nlink', 'state', 'feedback']
-    list_filter = ['state']
-    search_fields = ['nlink__owner__name', 'nlink__requestor__email']
-
-@admin.register(Archive)
-class ArchiveAdmin(admin.ModelAdmin):
-    list_display = ['negotiation', 'archived_at']
-    search_fields = ['negotiation__nlink__owner__name']
-
-@admin.register(SummaryStatistics)
-class SummaryStatisticsAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'request_count', 'average_response_time', 'last_updated']
-    search_fields = ['owner__name']
-    list_filter = ['last_updated']
+    list_display = ['requestor_id', 'requestor_email', 'otp_expiray', 'is_verified']
+    search_fields = ['requestor_email']
+    list_filter = ['otp_expiray', 'is_verified']
 
 @admin.register(Questionnaire)
 class QuestionnaireAdmin(admin.ModelAdmin):
-    list_display = ['questionnaire_id', 'owner']  # This works as Questionnaire model has owner as a ForeignKey
+    list_display = ['questionnaire_id', 'owner', 'is_submitted']
     search_fields = ['questionnaire_id', 'owner__email']
+    list_filter = ['is_submitted']
+
+@admin.register(NLink)
+class NLinkAdmin(admin.ModelAdmin):
+    list_display = [
+        'link_id', 'negotiation_id', 'owner_id', 'requestor_id', 
+        'questionnaire_SAID', 'expiration_date', 'last_activity'
+    ]
+    search_fields = ['owner_id__email', 'requestor_id__requestor_email']
+    list_filter = ['expiration_date', 'last_activity']
+
+@admin.register(Negotiation)
+class NegotiationAdmin(admin.ModelAdmin):
+    list_display = ['negotiation_id', 'negotiation_status', 'questionnaire_SAID', 'timestamps']
+    list_filter = ['negotiation_status', 'timestamps']
+    search_fields = ['questionnaire_SAID__questionnaire_id']
+
+@admin.register(Archive)
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ['negotiation_id', 'archived_timestamp']
+    search_fields = ['negotiation_id__negotiation_status']
+
+@admin.register(SummaryStatistics)
+class SummaryStatisticsAdmin(admin.ModelAdmin):
+    list_display = ['owner_id', 'summary_date']
+    search_fields = ['owner_id__email']
+    list_filter = ['summary_date']
