728dd881a437fe047a2e1b9e1a91760df8af42e4
Setayesh Sanavi <ssanavi@uoguelph.ca>
2024-10-18 14:17:28 -0400

    link table look up added.

--- drt/urls.py
+++ drt/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('request-access/', views.request_access, name='request_access'),
     path('fill-questionnaire/<uuid:uuid>/', views.fill_questionnaire, name='fill_questionnaire'),
 
-    path('generate_nlinks/', views.generate_nlinks, name='generate_nlinks'),
+    path('generate_nlinks/<str:link_id>/', views.generate_nlinks, name='generate_nlinks'),
     path('verify/requestor/<str:link_id>/', views.requestor_email_entry, name='requestor_email_entry'),
     path('verify/otp/<str:link_id>/', views.verify_otp, name='verify_otp'),
 ]
