4ddeb30a6152d4d4f060fde5387bac64bc73e05b
olichose123 <olivier@banville.net>
2024-11-14 13:40:50 -0500

    configure default settings

--- drt_core/wsgi.py
+++ drt_core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drt.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drt.settings.local")
 
 application = get_wsgi_application()
