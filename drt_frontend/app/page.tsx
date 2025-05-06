d4e887a6c3f2709ce83a3be7a34fd6f9e5c00f00
Setayesh Sanavi <ssanavi@uoguelph.ca>
2025-05-06 16:48:22 -0400

    fix the questionnaire

--- drt_frontend/app/page.tsx
+++ drt_frontend/app/page.tsx
@@ -1,15 +1,33 @@
-import Form from './components/Form/Form'
-import './globals.css';
+// "use client";
 
-export default function Home() {
+
+// import Form from './components/Form/Form'
+// import './globals.css';
+
+// export default function Home() {
+//   return (
+//     <section className='py-24'>
+//       <div className='container font-sans text-gray-800'>
+//         <Form 
+//           onSave={() => console.log('Save action triggered')} 
+//           onSubmit={() => console.log('Submit action triggered')} 
+//         />
+//       </div>
+//     </section>
+//   )
+// }
+
+// pages/index.tsx
+import React from 'react';
+import JSONRenderer from './components/JSONRenderer';
+
+const Home: React.FC = () => {
   return (
-    <section className='py-24'>
-      <div className='container font-sans text-gray-800'>
-        <Form 
-          onSave={() => console.log('Save action triggered')} 
-          onSubmit={() => console.log('Submit action triggered')} 
-        />
-      </div>
-    </section>
-  )
-}
+    <div>
+      <h1>Dynamic Questionnaire</h1>
+      <JSONRenderer />
+    </div>
+  );
+};
+
+export default Home;
