b8d89a952f19f0d0e9ed5b61905e96055be4f73a
Setayesh Sanavi <ssanavi@uoguelph.ca>
2025-05-06 16:24:37 -0400

    fix form input

--- drt_frontend/app/page.tsx
+++ drt_frontend/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container font-sans text-gray-800'>
-        <Form />
+        <Form 
+          onSave={() => console.log('Save action triggered')} 
+          onSubmit={() => console.log('Submit action triggered')} 
+        />
       </div>
     </section>
   )
