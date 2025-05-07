cab8c6816d34848acdc9482bfb1d1008c47e83df
Setayesh Sanavi <ssanavi@uoguelph.ca>
2025-05-07 09:50:45 -0400

    route redirection fixed.

--- drt_frontend/app/page.tsx
+++ drt_frontend/app/page.tsx
@@ -1,33 +1,6 @@
-// "use client";
-
-
-// import Form from './components/Form/Form'
-// import './globals.css';
-
-// export default function Home() {
-//   return (
-//     <section className='py-24'>
-//       <div className='container font-sans text-gray-800'>
-//         <Form 
-//           onSave={() => console.log('Save action triggered')} 
-//           onSubmit={() => console.log('Submit action triggered')} 
-//         />
-//       </div>
-//     </section>
-//   )
-// }
-
-// pages/index.tsx
-import React from 'react';
-import JSONRenderer from './components/JSONRenderer';
-
-const Home: React.FC = () => {
-  return (
-    <div>
-      <h1>Dynamic Questionnaire</h1>
-      <JSONRenderer />
-    </div>
-  );
-};
-
-export default Home;
+// app/page.tsx
+'use client';
+import LoadDataPage from './datastore/load-data/page';
+export default function Root() {
+  return <LoadDataPage />;
+}
