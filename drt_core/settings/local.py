d9a52e6b576cf03b1c01184baccec3b2629aa4a9
Setayesh Sanavi <ssanavi@uoguelph.ca>
2025-07-30 14:24:30 -0400

    Fix Secret Leak.

--- drt_core/settings/local.py
+++ drt_core/settings/local.py
@@ -10,8 +10,6 @@ print("▶︎ frontend base url =", os.getenv("FRONTEND_BASE_URL"))
 
 # We open everything on local mode
 ALLOWED_HOSTS = ["*"]
-# CSRF_COOKIE_SAMESITE = "None"
-# CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ["http://*"]
 
 # during local dev over HTTP, cookies must NOT be "secure-only"
@@ -35,56 +33,9 @@ CSRF_TRUSTED_ORIGINS = [
 # CSRF_COOKIE_SECURE = True
 
 
-# INSTALLED_APPS  ["anymail"]
-
-# EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-# ANYMAIL = {
-#     "MAILGUN_API_KEY": os.environ.get("MAILGUN_API_KEY"),
-#     "MAILGUN_SENDER_DOMAIN": "sandbox52c30de58b9b48c2925e0795c53759c9.mailgun.org",
-# }
-# DEFAULT_FROM_EMAIL = "DART System <postmaster@sandbox52c30de58b9b48c2925e0795c53759c9.mailgun.org>"
-
-
-# # Use SendGrid's Web API backend
-# EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-# SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-
-# # Standard "from" address must match your Single-Sender
-# DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
-
-# # Use SendGrid's SMTP backend (optional)
-# EMAIL_BACKEND      = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST         = "smtp.sendgrid.net"
-# EMAIL_PORT         = 587
-# EMAIL_HOST_USER    = "apikey"
-# EMAIL_HOST_PASSWORD= os.environ.get("SENDGRID_API_KEY")
-# EMAIL_USE_TLS      = True
-# DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
-
-
-# settings.py
-
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# DEFAULT_FROM_EMAIL = 'no-reply@yourdomain.test'
-
-# if ENVIRONMENT == 'staging':
-#     EMAIL_HOST         = 'sandbox.smtp.mailtrap.io'
-#     EMAIL_HOST_USER    = os.environ.get('EMAIL_HOST_USER')
-#     EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_HOST_PASSWORD')
-#     EMAIL_PORT         = 2525
-#     EMAIL_USE_TLS      = True
-# elif ENVIRONMENT == 'production':
-#     EMAIL_HOST         = 'smtp.mailgun.org'
-#     EMAIL_HOST_USER    = os.environ.get('MAILGUN_SMTP_LOGIN')
-#     EMAIL_HOST_PASSWORD= os.environ.get('MAILGUN_SMTP_PASSWORD')
-#     EMAIL_PORT         = 587
-#     EMAIL_USE_TLS      = True
-
-
-# settings.py
 if ENVIRONMENT == "staging":
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.ethereal.email"
@@ -141,53 +92,3 @@ STATIC_URL = "/static/"
 STATIC_ROOT = "/usr/src/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/usr/src/media/"
-
-
-# from .base import *
-
-# DEBUG = "True"
-# # We open everything on local mode
-# ALLOWED_HOSTS = ["*"]
-# CSRF_COOKIE_SAMESITE = "None"
-# CSRF_COOKIE_SECURE = True
-# CSRF_TRUSTED_ORIGINS = ["http://*"]
-
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # in console for now
-# DEFAULT_FROM_EMAIL = 'sanavisetayesh@gmail.com'
-
-
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',  # Frontend origin
-# ]
-# FRONTEND_BASE_URL = "http://localhost:3000"
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "Ss12345678",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     },
-# }
-
-
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': 'redis://127.0.0.1:6379/1',
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#         }
-#     }
-# }
-
-# # Storage, static and media
-# STATIC_URL = "/static/"
-# STATIC_ROOT = "/usr/src/static/"
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = "/usr/src/media/"
